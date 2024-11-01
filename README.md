2c1f945942731f17e928da4ae744aa0aa47c327a
Alberno <hitdeveloper2023@gmail.com>
2024-11-01 10:56:00 -0400

    ALG MD

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
-# ALG-MD
-Un projet Whatsapp bot en cours de developement
+![Typing SVG](https://readme-typing-svg.demolab.com?font=Ribeye&size=50&pause=1000&color=3F00FF&center=true&width=900&height=100&lines=𝐑𝐎𝐌𝐄𝐊%20-𝐗𝐃;%20𝗠𝗨𝗟𝗧𝗜-𝗗𝗘𝗩𝗜𝗖𝗘%20𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣%20𝗕𝗢𝗧;%20𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗗%20𝗕𝗬%20𝐑𝐎𝐌𝐄𝐊%20𝐗𝐃..💖)
+<p align="center">
+
+</p>
+
+
+<div align="center">
+</p
+
+<hr>
+<hr>
+<p align="center">
+<a href="https://github.com/Alberlegrand/ALG-MD">
+    <img src="https://files.catbox.moe/hg86c0.jpg">
+  </a>
+
+
+    
+
+![forks](https://img.shields.io/github/forks/Alberlegrand/ALG-MD?label=Forks&style=social)
+
+![stars](https://img.shields.io/github/stars/Alberlegrand/ALG-MD?style=social)
+
+
+
+
+<a href="https://https://whatsapp.com/channel/0029VakaPzeD38CV78dbGf0e"><img src="https://img.shields.io/badge/%E2%9D%A4%EF%B8%8F%E2%80%8D%20Join%20Our%20WhatsApp%20Channel%F0%9F%91%A8%E2%80%8D%F0%9F%92%BB-green" alt="📎 Join Our WhatsApp Channel" width="300"></a>
+
+
+
+## MY YT CHANNEL
+
+[![Youtube](https://telegra.ph/file/eebe86c26e98ffeae39ea.jpg)](https://youtube.com/@ALG-MD9) 
+
+</details>
+
+
+
+
+
+<hr>
+
+<hr>
+1. FORK THIS REPO
+
+
+<a href='https://github.com/Alberlegrand/ALG-MD/fork' target="_blank"><img alt='Fork repo' src='https://img.shields.io/badge/Fork This Repo-black?style=for-the-badge&logo=git&logoColor=white'/></a>
+
+
+2. ## DEPLOY BY SESSION ID (WHATSAPP LOG WITH PAIR CODE)
+
+<a href='https://replit.com/@/https://replit.com/@salmanikhani426/ALG-MD-PAIR-1' target="_blank"><img alt='Get Session ID' src='https://img.shields.io/badge/%F0%9F%9A%80%EF%B8%8F%E2%80%8D%201-OUR%F0%9F%93%8B%20%20PAIR%20CODE%20WEB%F0%9F%91%A8%E2%80%8D%F0%9F%92%BB-yellow' width="400" height="50" alt="Deploy bot"/></a>
+
+<hr>
+<a href='https://replit.com/@salmanikhani426/ALG-MD-PAIR' target="_blank"><img alt='Session ID WEB' src='https://img.shields.io/badge/%F0%9F%9A%80%EF%B8%8F%E2%80%8D%202-OUR%F0%9F%93%8B%20%20PAIR%20CODE%20WEB%F0%9F%91%A8%E2%80%8D%F0%9F%92%BB-RED' width="400" height="50" alt="Deploy bot"/></a>
+
+<hr>
+<hr>
+
+## DEPLOYMENT METHODS
+3. ## Deploy to Koyeb ↓
+
+[![Deploy to Koyeb](https://www.koyeb.com/static/images/deploy/button.svg)](https://app.koyeb.com/deploy?name=ALG-MD&type=git&repository=Alberlegrand%2FALG-MD&branch=main&builder=dockerfile&env%5BAUTO_STATUS_REPLY%5D=true&env%5BSESSION_ID%5D=&env%5BAUTO_STATUS_SEEN%5D=true&env%5BAUTO_REACT%5D=true&ports=8000%3Bhttp%3B%2F)
+
+4. ## Deploy to RENDER ↓
+
+<a href="https://dashboard.render.com/" target="blank"><img align="center" src="https://telegra.ph/file/c15e952f017c10e12f431.jpg" width="100" height="20" alt="Deploy bot"/></a>
+
+5. ## Deploy to HEROKU
+
+[![Deploy to HEROKU](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?template=https://github.com/Alberlegrand/ALG-MD)
+
+
+
+   <a href="https://github.com/Alberlegrand/"><img src="https://files.catbox.moe/fgpxbl.jpg" width=80 height=80></a>   
+
+|**[`ALG-MD`](https://github.com/Alberlegrand)**|
+
+## 👑 `ALG-MD 𝐁𝐎𝐓 𝐎𝐖𝐍𝐄𝐑`👨 
+
+
+## DON'T FORGET TO TO BUY ALG COFFEE AND BOT UPDATES 🎉🎉
+- **Donate:** [Buy Me A Coffee](buymeacoffee.com/ALGbhai)
+- and check his bot ##🚀BOT🎉[https://github.com/ALG-Xsid/ALG-MD-V2)
+
+ <br>
+ </p>
+    <p align="center">
+<a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=EB+Garamond&weight=800&size=28&duration=4000&pause=1000&random=false&width=435&lines=THANKS+ALL+USE;MY+BOT+ALG-MD" alt="Typing SVG" /></a>
+<p>---
+README.MD FILE DONT COPY </p>
--- index.js
+++ index.js
@@ -1,60 +1,186 @@
-const config = require('./config');
-const { DisconnectReason, makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys');
-const handleCommand = require('./commands');
+import dotenv from 'dotenv';
+dotenv.config();
 
-async function connectToWhatsApp() {
+import {
+    makeWASocket,
+    Browsers,
+    fetchLatestBaileysVersion,
+    DisconnectReason,
+    useMultiFileAuthState,
+} from '@whiskeysockets/baileys';
+import { Handler, Callupdate, GroupUpdate } from './src/event/index.js';
+import express from 'express';
+import pino from 'pino';
+import fs from 'fs';
+import NodeCache from 'node-cache';
+import path from 'path';
+import chalk from 'chalk';
+import moment from 'moment-timezone';
+import axios from 'axios';
+import config from './config.cjs';
+import pkg from './lib/autoreact.cjs';
+const { emojis, doReact } = pkg;
+
+const sessionName = "session";
+const app = express();
+const orange = chalk.bold.hex("#FFA500");
+const lime = chalk.bold.hex("#32CD32");
+let useQR = true;
+let initialConnection = true;
+const PORT = process.env.PORT || 3000;
+
+const MAIN_LOGGER = pino({
+    timestamp: () => `,"time":"${new Date().toJSON()}"`
+});
+const logger = MAIN_LOGGER.child({});
+logger.level = "trace";
+
+const msgRetryCounterCache = new NodeCache();
+
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
+const sessionDir = path.join(__dirname, 'session');
+const credsPath = path.join(sessionDir, 'creds.json');
+
+if (!fs.existsSync(sessionDir)) {
+    fs.mkdirSync(sessionDir, { recursive: true });
+}
+
+async function downloadSessionData() {
+    if (!config.SESSION_ID) {
+        console.error('Please add your session to SESSION_ID env !!');
+        return false;
+    }
+    const sessdata = config.SESSION_ID.split("ALG-MD&")[1];
+    const url = `https://pastebin.com/raw/${sessdata}`;
     try {
-        const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys');
-        const sock = makeWASocket({
-            auth: state,
-            printQRInTerminal: true
-        });
-
-        sock.ev.on('creds.update', saveCreds);
-
-        sock.ev.on('connection.update', async (update) => {
-            const { connection, lastDisconnect } = update;
-            if (connection === 'close') {
-                const shouldReconnect = (lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut;
-                console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect);
-                if (shouldReconnect) {
-                    connectToWhatsApp();
-                }
-            } else if (connection === 'open') {
-                console.log('opened connection');
-                const myProfileJid = config.owner + '@s.whatsapp.net';
-                const startMessage = `
-${config.botname} Connected
-
-Prefix  : [ ${config.prefix} ]
-Plugins : ${config.plugin}
-Mode    : Public
-Database: No DB
-  
-                `;
-                await sock.sendMessage(myProfileJid, { text: startMessage });
-            }
-        });
-
-        sock.ev.on('messages.upsert', async (m) => {
-            try {
-                console.log(JSON.stringify(m, undefined, 2));
-                const message = m.messages[0];
-                const remoteJid = message.key.remoteJid;
-                const messageText = message.message?.conversation || message.message?.extendedTextMessage?.text;
-
-                if (messageText && messageText.startsWith(config.HANDLERS)) {
-                    await handleCommand(sock, message, messageText.slice(config.HANDLERS.length).trim());
-                }
-            } catch (error) {
-                console.error('Error in message processing:', error);
-                await sock.sendMessage(message.key.remoteJid, { text: 'An error occurred while processing your message.' });
-            }
-        });
+        const response = await axios.get(url);
+        const data = typeof response.data === 'string' ? response.data : JSON.stringify(response.data);
+        await fs.promises.writeFile(credsPath, data);
+        console.log("🔒 Session Successfully Loaded !!");
+        return true;
     } catch (error) {
-        console.error('Error in WhatsApp connection:', error);
+       // console.error('Failed to download session data:', error);
+        return false;
     }
 }
 
-// Lancer la connexion à WhatsApp
-connectToWhatsApp();
+async function start() {
+    try {
+        const { state, saveCreds } = await useMultiFileAuthState(sessionDir);
+        const { version, isLatest } = await fetchLatestBaileysVersion();
+        console.log(`🤖 ALG-MD using WA v${version.join('.')}, isLatest: ${isLatest}`);
+        
+        const Matrix = makeWASocket({
+            version,
+            logger: pino({ level: 'silent' }),
+            printQRInTerminal: useQR,
+            browser: ["ALG-MD", "safari", "3.3"],
+            auth: state,
+            getMessage: async (key) => {
+                if (store) {
+                    const msg = await store.loadMessage(key.remoteJid, key.id);
+                    return msg.message || undefined;
+                }
+                return { conversation: "ALG-MD whatsapp user bot" };
+            }
+        });
+
+        Matrix.ev.on('connection.update', (update) => {
+            const { connection, lastDisconnect } = update;
+            if (connection === 'close') {
+                if (lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut) {
+                    start();
+                }
+            } else if (connection === 'open') {
+                if (initialConnection) {
+                    console.log(chalk.green("😃ALG-MD-CONNECTED Successful️✅ JOIN FOR MORE UPDATE🥏 https://whatsapp.com/channel/0029VakaPzeD38CV78dbGf0e"));
+                    Matrix.sendMessage(Matrix.user.id, { text: `😃ALG-MD-CONNECTED Successful️✅ JOIN FOR MORE UPDATE🥏 https://whatsapp.com/channel/0029VakaPzeD38CV78dbGf0e` });
+                    initialConnection = false;
+                } else {
+                    console.log(chalk.blue("♻️ Connection reestablished after restart."));
+                }
+            }
+        });
+
+        Matrix.ev.on('creds.update', saveCreds);
+
+        Matrix.ev.on("messages.upsert", async chatUpdate => await Handler(chatUpdate, Matrix, logger));
+        Matrix.ev.on("call", async (json) => await Callupdate(json, Matrix));
+        Matrix.ev.on("group-participants.update", async (messag) => await GroupUpdate(Matrix, messag));
+
+        if (config.MODE === "public") {
+            Matrix.public = true;
+        } else if (config.MODE === "private") {
+            Matrix.public = false;
+        }
+
+        Matrix.ev.on('messages.upsert', async (chatUpdate) => {
+            try {
+                const alg = chatUpdate.messages[0];
+                console.log(alg);
+                if (!alg.key.fromMe && config.AUTO_REACT) {
+                    console.log(alg);
+                    if (alg.message) {
+                        const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+                        await doReact(randomEmoji, alg, Matrix);
+                    }
+                }
+            } catch (err) {
+                console.error('Error during auto reaction:', err);
+            }
+        });
+        
+        Matrix.ev.on('messages.upsert', async (chatUpdate) => {
+    try {
+        const alg = chatUpdate.messages[0];
+        const fromJid = alg.key.participant || alg.key.remoteJid;
+        if (!alg || !alg.message) return;
+        if (alg.key.fromMe) return;
+        if (alg.message?.protocolMessage || alg.message?.ephemeralMessage || alg.message?.reactionMessage) return; 
+        if (alg.key && alg.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_SEEN) {
+            await Matrix.readMessages([alg.key]);
+            
+            if (config.AUTO_STATUS_REPLY) {
+                const customMessage = config.STATUS_READ_MSG || '✅ Auto Status Seen Bot By ALG-MD';
+                await Matrix.sendMessage(fromJid, { text: customMessage }, { quoted: alg });
+            }
+        }
+    } catch (err) {
+        console.error('Error handling messages.upsert event:', err);
+    }
+});
+
+    } catch (error) {
+        console.error('Critical Error:', error);
+        process.exit(1);
+    }
+}
+
+async function init() {
+    if (fs.existsSync(credsPath)) {
+        console.log("🔒 Session file found, proceeding without QR code.");
+        await start();
+    } else {
+        const sessionDownloaded = await downloadSessionData();
+        if (sessionDownloaded) {
+            console.log("🔒 Session downloaded, starting bot.");
+            await start();
+        } else {
+            console.log("No session found or downloaded, QR code will be printed for authentication.");
+            useQR = true;
+            await start();
+        }
+    }
+}
+
+init();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
