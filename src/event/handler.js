585e5914a483c52e855c4e33444234df9921192d
Alberlegrand <75179569+Alberlegrand@users.noreply.github.com>
2024-11-01 15:32:33 -0400

    Update handler.js

--- src/event/handler.js
+++ src/event/handler.js
@@ -1,7 +1,7 @@
 import { serialize, decodeJid } from '../../../ALG-MD/lib/Serializer.js';
 import path from 'path';
 import fs from 'fs/promises';
-import config from '../../ALG-MD-main/config.cjs';
+import config from '../../config.cjs';
 import { smsg } from '../../../ALG-MD-main/lib/myfunc.cjs';
 import { handleAntilink } from './antilink.js';
 import { fileURLToPath } from 'url';
