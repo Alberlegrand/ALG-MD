f576b94031b2acff609fcd2897e98b4a18a3334e
Alberlegrand <75179569+Alberlegrand@users.noreply.github.com>
2024-11-03 13:58:07 -0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -16,8 +16,8 @@ const config = {
   AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
    /*auto block only for 212 */
   AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : true,
-  
-  
+ADMIN_NUMBER: process.env.ADMIN_NUMBER || "50947989665"
+
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
