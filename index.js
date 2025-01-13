7178380d51eb743bdb84f8c9f864c7bd0967092a
HAITIAN IT Developer <142026019+HITDeveloper2023@users.noreply.github.com>
2025-01-13 17:29:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function downloadSessionData() {
         console.log("🔒 Session Successfully Loaded !!");
         return true;
     } catch (error) {
-        // console.error('Failed to download session data:', error);
+         console.error('Failed to download session data:', error);
         return false;
     }
 }
@@ -145,31 +145,31 @@ async function start() {
         });
 
         Matrix.ev.on('messages.upsert', async (update) => {
-  const msg = update.messages[0];
+            const msg = update.messages[0];
 
-  // Vérifiez si le message vient des statuts
-  if (msg.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_LIKE) {
-    const me = await Matrix.user.id;
+            // Vérifiez si le message vient des statuts
+            if (msg.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_LIKE) {
+                const me = await Matrix.user.id;
 
-    // Tableau d'emojis pour les réactions aléatoires (plus de 20)
-    const emojis = [
-      '💚', '🔥', '😊', '🎉', '👍', '💫', '🥳', '✨', 
-      '😎', '🌟', '❤️', '😂', '🤔', '😅', '🙌', '👏',
-      '💪', '🤩', '🎶', '💜', '👀', '🤗', '🪄', '😋',
-      '🤝', '🥰', '😻', '🆒', '🙈', '😇','🎈','😇','🥳','🧐','🥶','☠️','🤓','🤖','👽','🐼','🇭🇹'
-    ];
+                // Tableau d'emojis pour les réactions aléatoires (plus de 20)
+                const emojis = [
+                    '💚', '🔥', '😊', '🎉', '👍', '💫', '🥳', '✨',
+                    '😎', '🌟', '❤️', '😂', '🤔', '😅', '🙌', '👏',
+                    '💪', '🤩', '🎶', '💜', '👀', '🤗', '🪄', '😋',
+                    '🤝', '🥰', '😻', '🆒', '🙈', '😇', '🎈', '😇', '🥳', '🧐', '🥶', '☠️', '🤓', '🤖', '👽', '🐼', '🇭🇹'
+                ];
 
-    // Choisir un emoji aléatoire
-    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+                // Choisir un emoji aléatoire
+                const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
 
-    // Envoyer la réaction
-    await Matrix.sendMessage(
-      msg.key.remoteJid,
-      { react: { key: msg.key, text: randomEmoji } },
-      { statusJidList: [msg.key.participant, me] }
-    );
-  }
-});
+                // Envoyer la réaction
+                await Matrix.sendMessage(
+                    msg.key.remoteJid,
+                    { react: { key: msg.key, text: randomEmoji } },
+                    { statusJidList: [msg.key.participant, me] }
+                );
+            }
+        });
 
 
         Matrix.ev.on('messages.upsert', async (chatUpdate) => {
